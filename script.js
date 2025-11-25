f30e18a7ec80eebb6aa9cd6331424d81851ff80c
Denilsonsiewert <39243863+Denilsonsiewert@users.noreply.github.com>
2025-11-24 23:13:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,17 @@
 (function(){
   const select = document.getElementById('refreshInterval');
   let timer = null;
-
   function applyInterval(val){
     const seconds = parseInt(val, 10) || 0;
     if (timer) clearInterval(timer);
-    if (seconds > 0) {
-      timer = setInterval(() => location.reload(), seconds * 1000);
-    }
+    if (seconds > 0) timer = setInterval(() => location.reload(), seconds * 1000);
   }
-  select.addEventListener('change', (e) => applyInterval(e.target.value));
+  select.addEventListener('change', e => applyInterval(e.target.value));
   applyInterval(select.value);
 
-  // Botão de tela cheia
-  const fsBtn = document.getElementById('fullscreenBtn');
-  fsBtn.addEventListener('click', () => {
+  document.getElementById('fullscreenBtn').addEventListener('click', () => {
     const el = document.documentElement;
-    if (!document.fullscreenElement) {
-      if (el.requestFullscreen) el.requestFullscreen();
-    } else {
-      if (document.exitFullscreen) document.exitFullscreen();
-    }
+    if (!document.fullscreenElement) el.requestFullscreen();
+    else document.exitFullscreen();
   });
 })();
